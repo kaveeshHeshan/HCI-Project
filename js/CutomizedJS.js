3056f6fa72a2aad5162f00293fb93d2457a68309
KshanHeshan <kaveeshkshan@gmail.com>
2019-09-16 13:15:25 +0530

    Changed some margins and paddings

---
+++ js/CutomizedJS.js
@@ -0,0 +1,17 @@
+// Scroll top functions (topBtn's functions)
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 40) {
+            $('#topBtn').fadeIn();
+        } else{
+            $('#topBtn').fadeOut();
+        }
+    });
+
+    $("#topBtn").click(function(){
+        $('html,body').animate({scrollTop : 0}, 800);
+    });
+});
+
+// 
